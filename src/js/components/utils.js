179336f37a558bd27957435e00f4525a7df1673f
gregoiremielle <gregoiremielle@gmail.com>
2017-11-05 22:49:53 +0100

    ✨ Add IntersectionObserver

--- src/js/components/utils.js
+++ src/js/components/utils.js
@@ -1,4 +1,21 @@
+import 'intersection-observer';
+
 const map = (value, istart, istop, ostart, ostop) =>
   ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
 
-export { map };
+const intersectionObserver = (element, callback) => {
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.intersectionRatio < 1) return;
+      callback();
+      observer.disconnect();
+    },
+    {
+      threshold: 1
+    }
+  );
+
+  observer.observe(element);
+};
+
+export { map, intersectionObserver };
